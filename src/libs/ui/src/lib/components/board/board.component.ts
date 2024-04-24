36fd2ddd48bc954bbcf5aa731e35743149d7ba5a
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-04-24 17:15:38 +0530

    ui lib: board

---
+++ src/libs/ui/src/lib/components/board/board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-board',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss',
+})
+export class BoardComponent {}
